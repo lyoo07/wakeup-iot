c24b4f88ea90174ab81f9dc25a7e6605dfab2e16
lyoo07 <246669469+lyoo07@users.noreply.github.com>
2025-12-26 23:34:45 +0900

    ボタン追加

--- main.py
+++ main.py
@@ -3,22 +3,47 @@ import os
 import time
 from datetime import datetime
 
-BUTTON_PIN = 17  # GPIO17
+# GPIO設定
+ALARM_STOP_BTN = 17
+MUSIC_BTN = 27
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(ALARM_STOP_BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(MUSIC_BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-print("起きましたボタン待機中...")
+ALARM_TIME = "07:00"
+ALARM_SOUND = "hituzi.mp3"
+MUSIC_SOUND = "music.mp3"
+
+alarm_playing = False
+alarm_process = None
 
 try:
     while True:
-        if GPIO.input(BUTTON_PIN) == GPIO.LOW:
-            filename = datetime.now().strftime("%Y%m%d_%H%M%S.jpg")
-            os.system(f"fswebcam photos/{filename}")
-            print(f"撮影しました: {filename}")
+        now = datetime.now().strftime("%H:%M")
+
+        if now == ALARM_TIME and not alarm_playing:
+            print("アラーム開始")
+            alarm_process = os.system(f"mpg-123 -q {ALARM_SOUND} &")
+            alarm_playing = True
+
+        if GPIO.input(ALARM_STOP_BTN) == GPIO.LOW and alarm_playing:
+            print("アラーム停止")
+            os.system("pkill mpg123")
+            alarm_playing = False
             time.sleep(1)
-        time.sleep(0.1)
+
+        if GPIO.input(MUSIC_BTN) == GPIO.LOW:
+            print("音楽再生")
+            os.system("pkill mpg123")
+            os.system(f"mpg123 {MUSIC_SOUND} &")
+            time.sleep(1)
+
+        time.sleep(0.2)
+
+
 
 except KeyboardInterrupt:
     GPIO.cleanup()
+    os.system("pkill mpg123")
     print("終了しました")
